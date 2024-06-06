087bdab7660841a74c6c20f2b0695ddf4bc48f74
luciorim <88190686+luciorim@users.noreply.github.com>
2024-06-06 19:34:11 +0500

    initial structure

---
+++ ship-service/src/main/java/com/luciorim/ship/ShipServiceApplication.java
@@ -0,0 +1,13 @@
+package com.luciorim.ship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShipServiceApplication.class, args);
+    }
+
+}
---
+++ ship-service/src/test/java/com/luciorim/ship/ShipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciorim.ship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
